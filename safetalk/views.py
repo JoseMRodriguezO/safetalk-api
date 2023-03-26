411da7da06996b9b95909c35df3eef06917f95bc
JoseMRodriguez <Josemiguelrodriguez89@gmail.com>
2023-03-26 13:39:36 -0500

    Add index action for both tables

--- safetalk/views.py
+++ safetalk/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from .models import Activity, Resource
 
 
-def index(request):
-    return JsonResponse({"message": "Hello from jose"})
+@csrf_exempt
+def activity_list(request):
+    if request.method == 'GET':
+        activities = Activity.objects.all()
+        data = {'activities': list(activities.values())}
+        return JsonResponse(data)
+    elif request.method == 'POST':
+        # code to create a new activity
+        pass
+
+
+@csrf_exempt
+def resource_list(request):
+    if request.method == 'GET':
+        resources = Resource.objects.all()
+        data = {'resources': list(resources.values())}
+        return JsonResponse(data)
+    elif request.method == 'POST':
+        # code to create a new resource
+        pass

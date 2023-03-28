2bcb1e44ee9569a49d656fdfeec38d059e2b75cb
JoseMRodriguez <Josemiguelrodriguez89@gmail.com>
2023-03-27 22:48:13 -0500

    Final commit

--- mysite1/urls.py
+++ mysite1/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
 
 
     path('activities/', include("safetalk.urls")),
-
 
 ]
